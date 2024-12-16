612d8a10d3b44941cdb3779893a95dfae39abc03
Win Aung <91712429+winaung1@users.noreply.github.com>
2024-12-15 17:53:51 -0700

    done

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://financetracker-t0ag.onrender.com/api',  // Adjust the base URL to match your backend URL
+  baseURL: 'https://financetracker-zn8j.onrender.com/api',  // Adjust the base URL to match your backend URL
 });
 
 export default api;
